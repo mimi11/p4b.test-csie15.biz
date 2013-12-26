2ebd528b40794b3b5ac82d50c67b6df23b1908be
Mimi <carine.melhorn@gmail.com>
2013-12-26 16:36:19 -0500

    Update _v_template.php
    matching local and repository

--- views/_v_template.php
+++ views/_v_template.php
@@ -82,7 +82,7 @@
         </div><!--end footer right div-->
 
         <div id='footer_right'>
-            <p>attempting to fix git fetch .idea in my directory</p>
+          
             <p>Carine Melhorn</p>
             <p>c.melhorn@g.harvard.edu</p>
         </div><!--end footer_left div-->
